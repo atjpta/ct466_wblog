019d5f3b2e9229fa1b66bdec4ed3215c381ce554
atjpta <caoducan351@gmail.com>
2022-11-07 15:50:34 +0700

    dang lam

--- backend/app/controllers/search.controller.js
+++ backend/app/controllers/search.controller.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const db = require("../models");
 const Blog = db.blog;
 const user = db.user;
+const Question = db.question;
 
 
 exports.getListSearch = async (req, res, next) => {
@@ -90,6 +91,52 @@ exports.getListFollow = async (req, res, next) => {
         });
     } catch (error) {
         return next(res.status(500).send("lỗi khi getListFollow"))
+    }
+};
+
+
+
+exports.getListUser = async (req, res, next) => {
+    const { id } = req.params;
+    const condition = {
+        _id: id && mongoose.isValidObjectId(id) ? id : null,
+    };
+    try {
+        let listUser = await user.findOne(condition).select('follow')
+            .populate({
+                path: 'follow',
+                select: 'name avatar_Url id followBy repAnswer introduce',
+            })
+            .exec();
+        let slBlog = [];
+        let slQuestion = [];
+        let document = []
+        const end = listUser.follow.length
+        if (listUser) {
+            await listUser.follow.forEach(async (e, i) => {
+                slBlog = await Blog.find({ author: e._id });
+                slQuestion = await Question.find({ author: e._id });
+                
+                document.push({
+                    slBlog: slBlog.length,
+                    slQuestion: slQuestion.length,
+                    name: e.name,
+                    avatar_Url: e.avatar_Url,
+                    id: e._id,
+                    followBy: e.followBy,
+                    repAnswer: e.repAnswer,
+                    introduce: e.introduce,
+                })
+                if (document.length == end) {
+                    return res.send(document);
+                }
+            })
+        }
+        else {
+            return next(res.status(404).json({ Message: "không thể getListUser" }));
+        }
+    } catch (error) {
+        return next(res.status(500).send("lỗi khi getListUser" + error))
     }
 };
 
--- backend/app/routes/search.routes.js
+++ backend/app/routes/search.routes.js
@@ -8,6 +8,9 @@ module.exports = (app) => {
     
     router.route("/follow/:id")
         .get(search.getListFollow)
+    
+    router.route("/follow/user/:id")
+        .get(search.getListUser)
 
     app.use("/api/search", router);
 };
