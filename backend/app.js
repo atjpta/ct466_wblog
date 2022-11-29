250eb145aa5f7e1df234b643e3089b1c12fa2b18
atjpta <caoducan351@gmail.com>
2022-11-29 23:29:11 +0700

    fix6

--- backend/app.js
+++ backend/app.js
@@ -18,12 +18,32 @@ app.get("/", (req, res) => {
 
 route.Start(app);
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next(res.status(404).json({ message: "Resource not found" }))
-});
+
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
+
+const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+}
+
+
+
+app.use(allowCors(handler));
 app.use((error, req, res, next) => {
     if (res == null) {
         res.status(error.status || 500).json({ message: error.message || "internal Server Error" })
