ee6971843d80c7c17fdaabfcc9a22ae6ec54efee
atjpta <caoducan351@gmail.com>
2022-11-29 23:44:18 +0700

    fix8

--- backend/app.js
+++ backend/app.js
@@ -19,11 +19,11 @@ app.get("/", (req, res) => {
 route.Start(app);
 
 
-const allowCors = fn => async (req, res) => {
+const allowCors = fn => async (req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
     // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader(
         'Access-Control-Allow-Headers',
@@ -33,7 +33,8 @@ const allowCors = fn => async (req, res) => {
         res.status(200).end()
         return
     }
-    return await fn(req, res)
+    next();
+    return await fn(req, res, next)
 }
 
 const handler = (req, res) => {
