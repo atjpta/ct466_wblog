413a5c42dec9f478731d6734a8274dce70e90fbd
atjpta <caoducan351@gmail.com>
2022-11-30 00:12:04 +0700

    fix10

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({
-    limit: '50mb',
-    extended: true,
-})
+        limit: '50mb',
+        extended: true,
+    })
 );
 
 app.get("/", (req, res) => {
@@ -19,22 +19,37 @@ app.get("/", (req, res) => {
 route.Start(app);
 
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+app.use(handler(allowCors))
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
-    }
-    next();
+    res.header("Access-Control-Allow-Credentials", true)
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.header('Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    next(res.status(404).json({ message: "Resource not found" }))
 });
 app.use((error, req, res, next) => {
     if (res == null) {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,8 @@ export default defineConfig({
     port: 8081,
     proxy: {
       "/api": {
-        target: "https://api-wblog-atjpta.vercel.app/",
+        target: 'http://localhost:8088/',
+        // target: "https://api-wblog-atjpta.vercel.app/",
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, '')
       },
