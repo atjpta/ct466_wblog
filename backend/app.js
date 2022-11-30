b2a4ca5a9206d63e1de44ec5168410f48acd9a4c
atjpta <caoducan351@gmail.com>
2022-11-30 15:43:15 +0700

    fix12

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,12 @@ const cors = require("cors");
 const route = require('./app/routes')
 const app = express();
 
-app.use(cors());
+var corsOptions = {
+  origin: 'https://wblog-beta.vercel.app/'
+}
+
+
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({
         limit: '50mb',
